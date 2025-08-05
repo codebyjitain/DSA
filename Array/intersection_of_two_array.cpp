60e2814ca95ed0b4b4b18a0dc54aea67a29395b4
jitain <jitain6d@gmail.com>
2025-08-05 20:23:08 +0530

    Intersection Array Optimized Approach Two pointers

--- Array/intersection_of_two_array.cpp
+++ Array/intersection_of_two_array.cpp
@@ -8,18 +8,37 @@ vector<int> intersection_array(vector<int> arr1, vector<int> arr2)
     vector<int> ans;
 
     // first approach
-    for (int i = 0; i < arr1.size(); i++)
-    {
-        for (int j = 0; j < arr2.size(); j++)
-        {
-            if(arr1[i] == arr2[j]){
-                ans.push_back(arr1[i]);
-                arr2[j] = INT_MIN;
-            }
-        }
-    }
+    // for (int i = 0; i < arr1.size(); i++)
+    // {
+    //     for (int j = 0; j < arr2.size(); j++)
+    //     {
+    //         if(arr1[i] == arr2[j]){
+    //             ans.push_back(arr1[i]);
+    //             arr2[j] = INT_MIN;
+    //         }
+    //     }
+    // }
 
     // optimized approach -- write for it
+    int i = 0;
+    int j = 0;
+
+    while (i<arr1.size())   
+    {
+        if(arr1[i]<arr2[j]){
+            i++;
+        }
+        else if(arr1[i] == arr2[j]){
+            ans.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+        else
+        {
+            j++;
+        }
+    }
+    
     
     return ans;
 }
@@ -27,7 +46,7 @@ vector<int> intersection_array(vector<int> arr1, vector<int> arr2)
 int main()
 {
     vector<int> arr1 = {1, 2, 3, 4, 5, 6};
-    vector<int> arr2 = {7, 8, 9};
+    vector<int> arr2 = {1,2,3,7, 8, 9};
     vector<int> ans = intersection_array(arr1, arr2);
     print_array(ans);
     return 0;
