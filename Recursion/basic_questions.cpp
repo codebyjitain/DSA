3afe2afc5942492820355f1915b55bdfb5815ee0
jitain <jitain6d@gmail.com>
2025-08-20 13:46:20 +0530

    Basic Questions Based on Recursion

---
+++ Recursion/basic_questions.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+
+
+
+// Print NO. From N to 1
+// void printnum(int n){
+//     if(n==0){
+//         return;
+//     }
+
+//     cout<<n<<endl;
+//     printnum(n-1);
+// }
+
+
+// Print NO. From 1 to N
+// void printnum(int n){
+//     if(n==0){
+//         return;
+//     }
+
+//     printnum(n-1);
+//     cout<<n<<endl;
+// }
+
+
+// print Anything N-times
+// void print(int times){
+//     if(times == 0){
+//         return;
+//     }
+
+//     cout<<"Recursion" << endl;
+//     print(times-1);
+// }
+
+int main(){
+    // printnum(10);
+
+
+    // print(10);
+
+    return 0;
+}
