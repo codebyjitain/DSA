06c80ba1fe799fdb3ed9a094937d987e75d312c6
jitain <jitain6d@gmail.com>
2026-08-17 20:52:51 +0530

    practice graph

--- Graph/base.cpp
+++ Graph/base.cpp
@@ -3,29 +3,63 @@
 
 using namespace std;
 
-vector<vector<int>> matrixway(int n,int m){
-    vector<vector<int>> graph(n , vector<int>(n,0));
+// vector<vector<int>> matrixway(int n,int m){
+//     vector<vector<int>> graph(n , vector<int>(n,0));
 
-    for(int i = 0;i<m;i++){
+//     for(int i = 0;i<m;i++){
+//         int u,v;
+//         cin >> u >> v;
+//         graph[u][v] = 1;
+//         graph[v][u] = 1;
+//     }
+
+//     return graph;
+// }
+
+// vector<vector<int>> listway(int n,int m){
+//     vector<vector<int>> graph(n+1);
+    
+//     for (int i = 0; i < m; i++)
+//     {
+//         int u,v;
+//         cin>>u >> v;
+
+//         graph[u].push_back(v);
+//         graph[v].push_back(u);
+//     }
+//     return graph;
+// }
+
+
+// 17-08-2026 practice
+
+vector<vector<int>> matrixway(int n, int m){
+    vector<vector<int>> graph(n+1, vector<int>(n+1,0));
+
+    for (int i = 0; i < m; i++)
+    {
         int u,v;
-        cin >> u >> v;
+        cin>>u>>v;
+
         graph[u][v] = 1;
         graph[v][u] = 1;
+
     }
 
     return graph;
 }
 
-vector<vector<int>> listway(int n,int m){
+
+vector<vector<int>> listway(int n, int m){
     vector<vector<int>> graph(n+1);
-    
-    for (int i = 0; i < m; i++)
-    {
+
+    for(int i = 0;i<m;i++){
         int u,v;
-        cin>>u >> v;
+        cin>>u>>v;
 
         graph[u].push_back(v);
         graph[v].push_back(u);
+
     }
     return graph;
 }
